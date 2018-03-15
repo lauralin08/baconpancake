12f97c6b31a5a8c2c80157b9fc72dfdcdae7397f
lauralin08 <lauralin08@gmail.com>
2018-03-14 17:37:12 -0700

    commented out bodyParser

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
 app.use(express.static('views/images'));
 
 app.engine('handlebars', handlebars.engine);
